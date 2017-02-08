bad5593a1daa505c2a100b61a6fd79e29481821d
Andreas Caspers <aca@zuehlke.com>
2017-02-08 18:17:11 +0100

    Missing file added

---
+++ Controllers/NewEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalendarFunnel.Controllers
+{
+    public class NewEvent
+    {
+        public string Calendar { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+    }
+}
